c8c6f29c11def2eed4807f59bf0dbb6b858ab390
Jesse Frohlich <jesse@frohli.ch>
2022-05-25 14:36:58 -0400

    Add tests for closed_GDO functions.

--- test/closed_GDO.m
+++ test/closed_GDO.m
@@ -1,4 +1,22 @@
-VerificationTest[
-  Subscript[{}], (* TODO *)
-  {},
-TestID->"toMixed adds empty indices for closed components"]
+Module[
+  {i, j, L, Q, P},
+  VerificationTest[
+    getDomain@toMixed[Subscript[\[DoubleStruckCapitalE], {i}->{j}][L,Q,P]],
+    {{i},{}},
+TestID->"toMixed adds empty indices for closed components to domain"]]
+
+Module[
+  {i, j, L, Q, P},
+  VerificationTest[
+    getCodomain@toMixed[Subscript[\[DoubleStruckCapitalE], {i}->{j}][L,Q,P]],
+    {{j},{}},
+TestID->"toMixed adds empty indices for closed components to domain"]]
+
+Module[
+  { L, P, Q,
+    EE = Subscript[\[DoubleStruckCapitalE], {{1,2},{3,4}}->{{1},{2,3,4}}][L,Q,P]
+  },
+  VerificationTest[
+    toMixed@EE,
+    EE,
+TestID->"toMixed is null operation on expressions with closed domain"]]
