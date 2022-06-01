de0daf9e9f394c6b14aa73255bb801b1d3433d07
Jesse Frohlich <jesse@frohli.ch>
2022-06-01 16:47:00 -0400

    Update usage definition of `coinv`.

--- traceDegree.m
+++ traceDegree.m
@@ -51,11 +51,7 @@ Etest[n_] := Subscript[\[DoubleStruckCapitalE], {} -> Range[n]][
 ]
 
 
-(*
- * The coinvariants map defined on the (uncompleted) sl2-algebra expressed as a
- * non-standard multiplicative structure on a polynomial algebra.
- *)
-coinv::usage = "coinv[i][f] gives the coinvarant of expression f with respect to variables indexed by i. It returns a(n in)finite sum of monomials when given a(n in)finite sum."
+coinv::usage = "coinv[i][f] gives the coinvarant of expression in sl2+ f with respect to variables indexed by i. It returns a(n in)finite sum of monomials when given a(n in)finite sum."
 coinv[ii_][lincomb_Plus]:=coinv[ii]/@lincomb;
 coinv[ii_][word_]:=Module[{
         i,j,k,l,λ,
