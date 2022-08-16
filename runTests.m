ad0ca3d289f8406a0397e4b836556183656b1069
Jesse Frohlich <jesse@frohli.ch>
2022-08-16 18:14:29 -0400

    Add "TestsFailedWithMessages" to test suite report output.

--- runTests.m
+++ runTests.m
@@ -3,8 +3,11 @@ Get["StandardVersion.m"]
 Get["closed_GDO.m"]
 Get["equality_test.m"]
 Get["traceDegree.m"]
+Get["RVT.m"]
 reports=Module[{i},Table[TestReport[i],{i,FileNames["test/*.m"]}]];
 failedreports=Flatten@(Values@Values@#["TestsFailed"] & /@ reports);
+failedwithmessagesreports=Flatten@(Values@#["TestsFailedWithMessages"] & /@ reports);
+succeededreports=Flatten@(Values@#["TestsSucceeded"] & /@ reports);
 Print["Test report summary:"]
 Print[reports]
 Print["Failed tests summary:"]
