a7a8c7d63e49ec240fde788bfec4fb68c1b14c74
Jesse Frohlich <jesse@frohli.ch>
2022-10-20 17:13:22 -0400

    Implement `getFibres`.
    Consider just using `GatheBy`, as its implementation appears to be
    identical.

--- invariantComparison.m
+++ invariantComparison.m
@@ -1,3 +1,5 @@
 distinctValues::usage = "distinctValues[f][X] returns the size of f(X), that is,
 the number of distinct values f attains on X."
 distinctValues[f_][S_] := Length[Union[f/@S]]
+
+getFibres[f_][S_] := GatherBy[S,f]
