7b904716530ea3f35f67b016187d6cd9628250ba
Jesse Frohlich <jesse@frohli.ch>
2022-10-20 12:52:47 -0400

    Add tests for `subinvariantQ` (testing strict weakerness in invariants).

--- test/invariantComparison.m
+++ test/invariantComparison.m
@@ -3,7 +3,7 @@ Module[
                 f = Mod[#,2]&,
                 X = {0,1,2,3,4},
                 Y = {0,2,4},
-                Z = {},
+                Z = {}
         },
         VerificationTest[
                 distinctValues[f][X],
@@ -17,4 +17,29 @@ TestID->"distinctValues returns 1 on a fibre."];
                 distinctValues[f][Z],
                 0,
 TestID->"distinctValues returns the size of an empty image."]
+]
+
+Module[
+        {
+                mod4 = Mod[#,4]&,
+                mod2 = Mod[#,2]&,
+                X = {0,1,2,3,4},
+                Y = {0,4}
+        },
+        VerificationTest[
+                subinvariantQ[mod2][mod4][X],
+                True,
+TestID->"subinvariantQ compares a weaker invariant to a stronger one."];
+        VerificationTest[
+                subinvariantQ[mod2][mod4][Y],
+                True,
+TestID->"subinvariantQ compares equivalent invariants on a small domain."];
+        VerificationTest[
+                subinvariantQ[mod4][mod2][X],
+                False,
+TestID->"subinvariantQ compares a stronger invariant to a weaker one."];
+        VerificationTest[
+                subinvariantQ[mod4][mod2][Y],
+                True,
+TestID->"subinvariantQ does not distinguish invariants on a small domain."]
 ]
