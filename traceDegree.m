b6d16ecb1c2c1c489b3f31bd6dd42279b452b8b8
Jesse Frohlich <jesse@frohli.ch>
2022-10-26 18:43:46 -0400

    Fix implementation of `safeEval`.

--- traceDegree.m
+++ traceDegree.m
@@ -253,7 +253,7 @@ getxyCoef[i_][gdo_][bb_] :=
 
 safeEval[f_][x_] := Module[{fx, x0},
         If[(fx=Quiet[f[x]]) === Indeterminate,
-                Series[f[x0],x0 -> x]//Normal,
+                Series[f[x0],{x0,x,0}]//Normal,
                 fx
         ]
 ]
